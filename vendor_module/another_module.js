ffcb24c0969168c2ed174967ede951d57fa97d2b
刘力瑄 <liulixuan@sunlands.com>
2019-12-24 22:49:34 +0800

    del lodash

--- vendor_module/another_module.js
+++ vendor_module/another_module.js
@@ -1,9 +1,7 @@
 import * as React from 'react';
-import lodash from 'lodash';
 
 class Another extends React.Component {
     render() {
-        console.log(lodash.isEmpty([]));
         return (
             <div>another</div>
         )
--- vendor_module/index.js
+++ vendor_module/index.js
@@ -1,9 +1,7 @@
 import * as React from 'react';
-import lodash from 'lodash';
 
 class Index extends React.Component {
     render() {
-        console.log(lodash.isEmpty([]));
         return (
             <div>index</div>
         )
